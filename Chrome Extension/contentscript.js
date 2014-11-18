bad4c30a5befab9bd3868232240c4f12ba092725
BrOrlandi <brorlandi@gmail.com>
2014-11-18 10:12:04 -0200

    First Commit, Extension in development.

---
+++ Chrome Extension/contentscript.js
@@ -0,0 +1,45 @@
+var QueryString = function () {
+  // This function is anonymous, is executed immediately and 
+  // the return value is assigned to QueryString!
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    	// If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = pair[1];
+    	// If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]], pair[1] ];
+      query_string[pair[0]] = arr;
+    	// If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(pair[1]);
+    }
+  } 
+    return query_string;
+} ();
+
+Number.prototype.formatMoney = function(c, d, t){
+var n = this, 
+    c = isNaN(c = Math.abs(c)) ? 2 : c, 
+    d = d == undefined ? "." : d, 
+    t = t == undefined ? "," : t, 
+    s = n < 0 ? "-" : "", 
+    i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", 
+    j = (j = i.length) > 3 ? j % 3 : 0;
+   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+ };
+
+function start(){
+
+	id = QueryString.id;
+	if(id != undefined){
+		// get sal by id
+		professor = {name: "charler", salario: 2080.50}
+		sal = professor.salario.formatMoney();
+		var str = "<div class='salario_usp_info'>Salário: <span class='salario_usp_sal'>R$ "+sal+"</span></div>"
+	}
+	$(".flt-left.desc_pessoa h1").after(str);
+}();
